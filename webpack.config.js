57ca62baec04a53175dbc2653f63087ee34718e7
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2022-07-26 06:38:49 +0100

    Work with Asynchronous API, Create a StateApi that acts as the store, Pass initial data from server to client

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 module.exports = {
-  entry: './lib/components/Index.js',
+  resolve: {
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')],
+  },
+  entry: ['babel-polyfill', './lib/renderer/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
